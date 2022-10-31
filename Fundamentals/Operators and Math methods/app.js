79612e7cbab0d1912c094319794b6abeb4ff9793
Parviz <parviz.hajili@gmail.com>
2022-10-31 19:00:18 +0400

    Operators and Methods of the Math object

---
+++ Fundamentals/Operators and Math methods/app.js
@@ -0,0 +1,41 @@
+let value;
+
+
+const value1 = 10
+
+const value2 = 4
+
+// value = value1 + value2
+// value = value1 - value2
+// value = value1 * value2
+// value = value1 / value2
+// value = value1 % value2
+
+
+value = Math.PI;
+value = Math.E;
+
+value = Math.round(Math.PI)//3
+value = Math.round(3.6)//4
+
+value = Math.ceil(3.2)//4
+
+value = Math.floor(3.7) //3
+
+value = Math.sqrt(4)
+value = Math.sqrt(40)
+
+value = Math.abs(-5)//5
+
+value = Math.pow(5, 3)
+value = Math.pow(10, 6)
+
+value = Math.max(10, -1, 99, 55)
+value = Math.min(10, -1, 99, 55)
+
+value = Math.random()
+value = Math.random() * 35
+value = Math.round(Math.random() * 100 + 1)
+
+
+console.log(value)
