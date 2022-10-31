eab43f8896ea784f6991299be743dc6cb3e044c5
Parviz <parviz.hajili@gmail.com>
2022-10-31 20:37:48 +0400

    Some String Methods

---
+++ Fundamentals/String/app.js
@@ -0,0 +1,42 @@
+// let value;
+
+// const firstName = "Parviz"
+// const lastName = "Hajili"
+// const langs = "C#, Java, Python"
+
+
+// value = firstName + " " + lastName
+
+// value = firstName.length
+// value = firstName.concat(" ", lastName, " - ", ".NET Developer")
+
+// value = firstName.toLowerCase()
+// value = firstName.toUpperCase()
+
+// value = firstName[0]
+// value = lastName[4]
+// value = firstName[firstName.length - 1]
+
+//Index of
+
+// value = firstName.indexOf("r")
+// value = firstName.indexOf("z")
+// value = firstName.indexOf("E") //-1
+
+//Char at
+//value = firstName.charAt(0)//P
+
+
+//Split
+//value  = langs.split(",")
+
+//Replace
+//value = langs.replace("Python","Javascript")
+
+//Includes
+//value = langs.includes("Java")//true
+
+
+
+
+//console.log(value)
